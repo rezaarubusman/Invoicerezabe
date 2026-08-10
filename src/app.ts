835a88cbbb6d16770c28699c9f978867f40d862c
Fahreza Aqsa Arubusman <fahreza.arubusman@gmail.com>
2026-08-10 17:21:32 +0700

    setting file src (#2)
    Co-authored-by: rezaarubusman <fahreza.arubusman@gmail.comgit config --global user.name rezaarubusmangit config --global user.email fahreza.arubusman@gmail.com>

---
+++ src/app.ts
@@ -0,0 +1,74 @@
+import cors from "cors";
+import express from "express";
+import { corsOptions } from "./config/cors.js";
+import { loggerHttp } from "./lib/logger-http.js";
+import { errorMiddleware, notFoundMiddleware } from "./middlewares/error.middleware.js";
+import { prisma } from "./lib/prisma.js";
+import { AuthMiddleware } from "./middlewares/auth.middleware.js";
+import { UploadMiddleware } from "./middlewares/upload.middleware.js";
+import { ValidationMiddleware } from "./middlewares/validation.middleware.js";
+import { AuthController } from "./modules/auth/auth.controller.js";
+import { AuthRouter } from "./modules/auth/auth.router.js";
+import { AuthService } from "./modules/auth/auth.service.js";
+import { UsersService } from "./modules/users/users.service.js";
+import { UsersController } from "./modules/users/users.controller.js";
+import { UsersRouter } from "./modules/users/users.router.js";
+import { CloudinaryService } from "./modules/cloudinary/cloudinary.service.js";
+
+const PORT = 8000;
+
+export class App {
+  app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.configure();
+    this.registerModules();
+    this.handleError();
+  }
+
+  private configure = () => {
+    this.app.use(cors(corsOptions));
+    this.app.use(loggerHttp);
+    this.app.use(express.json());
+  };
+
+  private registerModules = () => {
+    // shared dependency
+    const prismaClient = prisma;
+
+    //middlewares
+    const authMiddleware = new AuthMiddleware(prismaClient);
+    const validationMiddleware = new ValidationMiddleware();
+    const uploadMiddleware = new UploadMiddleware();
+    const cloudinaryService = new CloudinaryService();
+
+    const authService = new AuthService(prismaClient);
+    const authController = new AuthController(authService);
+    const authRouter = new AuthRouter(authController, validationMiddleware, authMiddleware);
+
+    const usersService = new UsersService(prismaClient);
+    const usersController = new UsersController(usersService);
+    const usersRouter = new UsersRouter(usersController, validationMiddleware, authMiddleware);
+
+    this.app.use(
+      "/auth",
+      authRouter.getRouter()
+    );
+    this.app.use(
+      "/users",
+      usersRouter.getRouter()
+    );
+  };
+
+  private handleError = () => {
+    this.app.use(errorMiddleware);
+    this.app.use(notFoundMiddleware);
+  };
+
+  start() {
+    this.app.listen(PORT, () => {
+      console.log(`Server Running On Port : ${PORT}`);
+    });
+  }
+}
---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,75 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { Role } from "../../generated/prisma/enums.js";
+import { PrismaClient } from "../../generated/prisma/client.js";
+import { ApiError } from "../utils/api-error.js";
+
+interface JwtPayload {
+  id: string;
+  username: string;
+  role: Role;
+  sessionId: string;
+}
+
+export class AuthMiddleware {
+  constructor(private prisma: PrismaClient) {}
+
+  verifyToken = async(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const authHeader = req.headers.authorization;
+
+    if ( !authHeader || !authHeader.startsWith("Bearer ")) {
+      return next(new ApiError("No token provided", 401));
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+      const payload =
+        jwt.verify(
+          token,
+          process.env.JWT_ACCESS_SECRET as string
+        ) as JwtPayload;
+
+        const user = await this.prisma.user.findUnique({
+          where: { id: payload.id },
+          select: { activeSessionId: true}
+        });
+
+        if (!user || user.activeSessionId !== payload.sessionId) {
+          return next (new ApiError("This session has expired or you are login on other devices", 401));
+        }
+
+      res.locals.existingUser = payload;
+      res.locals.user = payload;
+
+      next();
+    } catch (err) {
+      if (err instanceof jwt.TokenExpiredError) {
+        return next( new ApiError("Token expired", 401));
+      }
+
+      return next(new ApiError("Token invalid", 401));
+    }
+  };
+
+  verifyRole = (...roles: Role[]) => {
+    return (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) => {
+      const userRole =
+        res.locals.existingUser?.role;
+
+      if (!userRole || !roles.includes(userRole)) {
+        return next(
+          new ApiError("You don't have access to this resource", 403));
+      }
+      next();
+    };
+  };
+};
