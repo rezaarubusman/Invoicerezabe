1860ecca69caf96ab45ddd172c78587e453090a7
rezaarubusman <fahreza.arubusman@gmail.comgit config --global user.name rezaarubusmangit config --global user.email fahreza.arubusman@gmail.com>
2026-08-17 10:50:05 +0700

    fix: updatemailservicev2

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -9,34 +9,34 @@ export class MailService {
 
   constructor() {
     this.transporter = createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
-        user: process.env.MAIL_USER, 
+        user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD,
       },
     });
-
-    this.transporter = createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASSWORD,
-  },
-  });
   }
 
-  private renderTemplates = async (templateName: string, context: object) => {
-    const __filename = fileURLToPath(import.meta.url); 
+  private renderTemplates = async (
+    templateName: string,
+    context: object,
+  ) => {
+    const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
 
     const templateDir = path.resolve(__dirname, "./tempelate");
 
-    const templatePath = path.join(templateDir, `${templateName}.hbs`);
+    const templatePath = path.join(
+      templateDir,
+      `${templateName}.hbs`,
+    );
 
-    const templateSource = await fs.readFile(templatePath, "utf-8");
+    const templateSource = await fs.readFile(
+      templatePath,
+      "utf-8",
+    );
 
     const compiledTemplate = handlebars.compile(templateSource);
 
@@ -48,14 +48,20 @@ export class MailService {
     subject: string,
     templateName: string,
     context: object,
-    attachments?: { filename: string; content: Buffer }[]
+    attachments?: {
+      filename: string;
+      content: Buffer;
+    }[],
   ) => {
-    const html = await this.renderTemplates(templateName, context);
+    const html = await this.renderTemplates(
+      templateName,
+      context,
+    );
 
     await this.transporter.sendMail({
-      to: to,
-      subject: subject,
-      html: html,
+      to,
+      subject,
+      html,
       attachments,
     });
   };
